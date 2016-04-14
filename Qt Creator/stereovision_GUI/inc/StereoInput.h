aca88eddfe9bfc1aca4683f4e519ac16b0c2e521
nicolasrosa <nicolas.rosa@hotmail.com>
2016-04-14 00:11:10 -0300

    [ADDED] Created StereoInput Class

---
+++ Qt Creator/stereovision_GUI/inc/StereoInput.h
@@ -0,0 +1,33 @@
+/*
+ * StereoInput.h
+ *
+ *  Created on: Apr 13, 2015
+ *      Author: nicolasrosa
+ */
+
+#ifndef STEREOINPUT_H
+#define STEREOINPUT_H
+
+/* Libraries */
+#include <string>
+
+using namespace std;
+
+class StereoInput{
+public:
+    /* Constructor and Destructor */
+    StereoInput();
+    ~StereoInput();
+
+//    /* Input files Path */
+//    string imageL_FileName;
+//    string imageR_FileName;
+
+//    /* Calibration files Path */
+//    string intrinsicsFileName;
+//    string extrinsicsFileName;
+
+    //enum{VideoFile,ImageFile} type;
+};
+
+#endif // STEREOINPUT_H
---
+++ Qt Creator/stereovision_GUI/src/StereoInput.cpp
@@ -0,0 +1,14 @@
+/*
+ * StereoFlags.h
+ *
+ *  Created on: Dec 3, 2015
+ *      Author: nicolasrosa
+ */
+
+/* Libraries */
+#include "inc/StereoInput.h"
+
+/* Constructor and Destructor */
+StereoInput::StereoInput(){}
+
+StereoInput::~StereoInput(){}
