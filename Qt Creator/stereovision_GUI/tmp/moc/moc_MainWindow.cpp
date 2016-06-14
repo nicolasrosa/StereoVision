580f1c67285b9b63088824198ef896d746dd919f
nicolasrosa <nicolas.rosa@hotmail.com>
2016-06-14 15:50:51 -0300

    [MODIFIED] Stereovision_GUI tmp files

--- Qt Creator/stereovision_GUI/tmp/moc/moc_MainWindow.cpp
+++ Qt Creator/stereovision_GUI/tmp/moc/moc_MainWindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'MainWindow.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.6.0)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'MainWindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.2.1. It"
+#error "This file was generated using the moc from 5.6.0. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -20,35 +20,36 @@
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
     QByteArrayData data[20];
-    char stringdata[524];
+    char stringdata0[523];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
-    offsetof(qt_meta_stringdata_MainWindow_t, stringdata) + ofs \
-        - idx * sizeof(QByteArrayData) \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
     )
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
-QT_MOC_LITERAL(0, 0, 10),
-QT_MOC_LITERAL(1, 11, 29),
-QT_MOC_LITERAL(2, 41, 0),
-QT_MOC_LITERAL(3, 42, 27),
-QT_MOC_LITERAL(4, 70, 30),
-QT_MOC_LITERAL(5, 101, 34),
-QT_MOC_LITERAL(6, 136, 34),
-QT_MOC_LITERAL(7, 171, 29),
-QT_MOC_LITERAL(8, 201, 36),
-QT_MOC_LITERAL(9, 238, 27),
-QT_MOC_LITERAL(10, 266, 30),
-QT_MOC_LITERAL(11, 297, 28),
-QT_MOC_LITERAL(12, 326, 7),
-QT_MOC_LITERAL(13, 334, 27),
-QT_MOC_LITERAL(14, 362, 33),
-QT_MOC_LITERAL(15, 396, 27),
-QT_MOC_LITERAL(16, 424, 5),
-QT_MOC_LITERAL(17, 430, 26),
-QT_MOC_LITERAL(18, 457, 33),
-QT_MOC_LITERAL(19, 491, 31)
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 29), // "stereoVisionProcess_UpdateGUI"
+QT_MOC_LITERAL(2, 41, 0), // ""
+QT_MOC_LITERAL(3, 42, 27), // "on_btnPauseOrResume_clicked"
+QT_MOC_LITERAL(4, 70, 30), // "on_btnShowDisparityMap_clicked"
+QT_MOC_LITERAL(5, 101, 34), // "on_btnShowStereoParamSetup_cl..."
+QT_MOC_LITERAL(6, 136, 34), // "on_btnShow3DReconstruction_cl..."
+QT_MOC_LITERAL(7, 171, 29), // "on_btnShowInputImages_clicked"
+QT_MOC_LITERAL(8, 201, 36), // "on_btnShowTrackingObjectView_..."
+QT_MOC_LITERAL(9, 238, 27), // "on_btnShowDiffImage_clicked"
+QT_MOC_LITERAL(10, 266, 30), // "on_btnShowWarningLines_clicked"
+QT_MOC_LITERAL(11, 297, 28), // "on_toggleBtnShowHist_clicked"
+QT_MOC_LITERAL(12, 326, 7), // "checked"
+QT_MOC_LITERAL(13, 334, 27), // "on_toggleBtnShowXYZ_toggled"
+QT_MOC_LITERAL(14, 362, 33), // "on_toggleBtnShowDispDepth_tog..."
+QT_MOC_LITERAL(15, 396, 27), // "on_methodSelector_activated"
+QT_MOC_LITERAL(16, 424, 5), // "index"
+QT_MOC_LITERAL(17, 430, 26), // "on_inputSelector_activated"
+QT_MOC_LITERAL(18, 457, 33), // "on_toggleBtnShowLeftImage_tog..."
+QT_MOC_LITERAL(19, 491, 31) // "on_toggleBtnShowOverlay_toggled"
+
     },
     "MainWindow\0stereoVisionProcess_UpdateGUI\0"
     "\0on_btnPauseOrResume_clicked\0"
@@ -65,7 +66,7 @@ QT_MOC_LITERAL(19, 491, 31)
     "on_methodSelector_activated\0index\0"
     "on_inputSelector_activated\0"
     "on_toggleBtnShowLeftImage_toggled\0"
-    "on_toggleBtnShowOverlay_toggled\0"
+    "on_toggleBtnShowOverlay_toggled"
 };
 #undef QT_MOC_LITERAL
 
@@ -83,22 +84,22 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   94,    2, 0x0a,
-       3,    0,   95,    2, 0x08,
-       4,    0,   96,    2, 0x08,
-       5,    0,   97,    2, 0x08,
-       6,    0,   98,    2, 0x08,
-       7,    0,   99,    2, 0x08,
-       8,    0,  100,    2, 0x08,
-       9,    0,  101,    2, 0x08,
-      10,    0,  102,    2, 0x08,
-      11,    1,  103,    2, 0x08,
-      13,    1,  106,    2, 0x08,
-      14,    1,  109,    2, 0x08,
-      15,    1,  112,    2, 0x08,
-      17,    1,  115,    2, 0x08,
-      18,    1,  118,    2, 0x08,
-      19,    1,  121,    2, 0x08,
+       1,    0,   94,    2, 0x0a /* Public */,
+       3,    0,   95,    2, 0x08 /* Private */,
+       4,    0,   96,    2, 0x08 /* Private */,
+       5,    0,   97,    2, 0x08 /* Private */,
+       6,    0,   98,    2, 0x08 /* Private */,
+       7,    0,   99,    2, 0x08 /* Private */,
+       8,    0,  100,    2, 0x08 /* Private */,
+       9,    0,  101,    2, 0x08 /* Private */,
+      10,    0,  102,    2, 0x08 /* Private */,
+      11,    1,  103,    2, 0x08 /* Private */,
+      13,    1,  106,    2, 0x08 /* Private */,
+      14,    1,  109,    2, 0x08 /* Private */,
+      15,    1,  112,    2, 0x08 /* Private */,
+      17,    1,  115,    2, 0x08 /* Private */,
+      18,    1,  118,    2, 0x08 /* Private */,
+      19,    1,  121,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void,
@@ -125,6 +126,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
         MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
         switch (_id) {
         case 0: _t->stereoVisionProcess_UpdateGUI(); break;
         case 1: _t->on_btnPauseOrResume_clicked(); break;
@@ -149,7 +151,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
 
 const QMetaObject MainWindow::staticMetaObject = {
     { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
-      qt_meta_data_MainWindow,  qt_static_metacall, 0, 0}
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
 };
 
 
@@ -160,8 +162,8 @@ const QMetaObject *MainWindow::metaObject() const
 
 void *MainWindow::qt_metacast(const char *_clname)
 {
-    if (!_clname) return 0;
-    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata))
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
         return static_cast<void*>(const_cast< MainWindow*>(this));
     return QMainWindow::qt_metacast(_clname);
 }
